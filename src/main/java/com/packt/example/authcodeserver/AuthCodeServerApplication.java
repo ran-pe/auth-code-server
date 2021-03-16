0dbcdde0a1ef1854dda9df01b2b3183bdec047de
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-16 17:28:59 +0900

    2021.03.16

--- src/main/java/com/packt/example/authcodeserver/AuthCodeServerApplication.java
+++ src/main/java/com/packt/example/authcodeserver/AuthCodeServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthCodeServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthCodeServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCodeServerApplication.class, args);
+    }
 
 }
--- src/main/java/com/packt/example/authcodeserver/api/UserProfile.java
+++ src/main/java/com/packt/example/authcodeserver/api/UserProfile.java
@@ -1,4 +1,12 @@
 package com.packt.example.authcodeserver.api;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserProfile {
+    private String name;
+    private String email;
 }

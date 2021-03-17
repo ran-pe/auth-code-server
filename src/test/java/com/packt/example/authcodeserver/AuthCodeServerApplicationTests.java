f49b970de15fd6e5e0ec1382084848bf65c08bb6
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-17 10:47:09 +0900

    2021.03.17

--- src/test/java/com/packt/example/authcodeserver/AuthCodeServerApplicationTests.java
+++ src/test/java/com/packt/example/authcodeserver/AuthCodeServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.packt.example.authcodeserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
